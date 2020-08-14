410413c2e8d13f9a102e327d313b70c48ce06f0f
Raquel Souto <kelsouto@gmail.com>
2020-08-14 00:49:26 -0300

    Adicionar o projeto relatorio-fiscal
    Foram adicionados os arquivos referentes ao projeto explicado no txt
    em anexo. O exercício foi realizado com o intuito de auxiliar no
    entendimento de POO (Programação Orientada a Objetos).

---
+++ relatorio-fiscal/src/Produto.java
@@ -0,0 +1,23 @@
+
+public class Produto {
+
+	private String descricao;
+	private double valorUnitario;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getValorUnitario() {
+		return this.valorUnitario;
+	}
+	
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+		
+}
